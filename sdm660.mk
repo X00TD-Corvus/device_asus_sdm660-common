86183e530c8eb3400af9048ecd6d35c3c8e74c69
harshhaareddy <harshhaa03@gmail.com>
2022-07-10 15:09:35 +0530

    sdm660-common: Drop Doze
    Signed-off-by: harshhaareddy <harshhaa03@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -155,22 +155,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@4.0-impl \
     android.frameworks.displayservice@1.0
 
-# Display interfaces
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.display.composer@1.0.vendor \
-    vendor.qti.hardware.display.composer@2.0.vendor \
-    vendor.qti.hardware.display.allocator-service \
-    vendor.qti.hardware.display.mapper@1.1.vendor \
-    vendor.qti.hardware.display.mapper@2.0.vendor \
-    vendor.qti.hardware.display.mapper@3.0.vendor \
-    vendor.qti.hardware.display.mapper@4.0.vendor \
-    vendor.qti.hardware.display.mapperextensions@1.0.vendor \
-    vendor.qti.hardware.display.mapperextensions@1.1.vendor
-
-# Doze mode
-PRODUCT_PACKAGES += \
-    Doze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
@@ -574,3 +558,6 @@ PRODUCT_COPY_FILES += \
 #PRODUCT_PACKAGES += \
 #   ZenParts
 
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+    
