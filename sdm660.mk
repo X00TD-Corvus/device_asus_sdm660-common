f8e20bf1aaec044cc68f658a1cf48d746f643d20
Albert I <kras@raphielgang.org>
2022-08-18 11:26:23 +0530

    sdm660-common: Drop modem-based assertions
    Signed-off-by: harshhaareddy <harshhaa03@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -428,9 +428,8 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     android.hardware.secure_element@1.2.vendor \
     android.hardware.radio@1.5.vendor \
-    android.hardware.radio.deprecated@1.0.vendor \
-    android.hardware.radio@1.6.vendor \
-    android.hardware.radio.config@1.3.vendor
+    android.hardware.radio.config@1.2.vendor \
+    android.hardware.radio.deprecated@1.0.vendor
 
 # Ramdisk
 PRODUCT_PACKAGES += \
