8f0a1f50a184a3cdb95aae7589836c4f27ba944b
Rajatkashyap154 <raj15400881@gmail.com>
2022-10-31 22:43:48 +0000

    EvoX

--- sdm660.mk
+++ sdm660.mk
@@ -321,7 +321,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-xtended
+    $(LOCAL_PATH)/overlay-evolution
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
